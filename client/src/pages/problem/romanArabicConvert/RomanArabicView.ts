19bebdc5655527e126264b130c1277367d384928
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-03-02 10:18:16 -0600

    Refactor and Update View, includes Table component

--- client/src/pages/problem/romanArabicConvert/RomanArabicView.ts
+++ client/src/pages/problem/romanArabicConvert/RomanArabicView.ts
@@ -2,14 +2,27 @@ import request, { type HTTPMethod } from '@/api/request';
 import { BaseView } from '@/base/BaseView';
 import { EndpointData } from '@/models/EndpointData';
 import { ConversionResult } from '@/models/romanArabicConvert/ConversionResult';
+import { ColumnDef, Table } from '@/ui/table';
 
 export class RomanArabicView extends BaseView {
   protected romanArabicSectionTag: HTMLElement;
+  protected tableContainer: HTMLDivElement;
+  protected table?: Table<ConversionResult>;
+
+  protected tableCols: ColumnDef<ConversionResult>[] = [
+    { header: 'Roman Numeral', key: 'romanNumeral' },
+    { header: 'Arabic Numeral', key: 'arabicNumeral' },
+    { header: 'Found in Cache?', key: 'foundInDB' },
+  ];
 
   constructor() {
     super();
     this.romanArabicSectionTag = document.createElement('section');
     this.romanArabicSectionTag.id = 'roman-arabic-container';
+    this.romanArabicSectionTag.classList.add('roman-arabic');
+
+    this.tableContainer = document.createElement('div');
+    this.tableContainer.id = 'table-container';
   }
 
   protected renderImpl(endpointData: EndpointData[]) {
@@ -18,14 +31,32 @@ export class RomanArabicView extends BaseView {
     const convertEndpoint = endpointData[0].endpoint;
     const flushEndpoint = endpointData[1].endpoint;
 
-    this.renderHeader(pageName, pageDescription);
-    this.renderConvertUI(convertEndpoint);
-    this.renderFlushUI(flushEndpoint);
+    this.resetSectionContainerHTML();
 
+    // Execute render methods in opposite order
+    // to place correctly in DOM
+    // the render methods `prepend` elements,
+    // not `append` or `appendChild` them
+    this.renderConversionUI(convertEndpoint, flushEndpoint);
+    this.renderHeader(pageName, pageDescription);
+
+    // Append Table Container to Section Container
+    this.romanArabicSectionTag.appendChild(this.tableContainer);
+
+    // Append Section Container to DOM
     super.appendContainerToDOM(this.romanArabicSectionTag);
+
+    // Initialize and Render Table
+    this.table = new Table({
+      containerId: this.tableContainer.id,
+      columnDefs: this.tableCols,
+    }).initialize([]);
   }
 
-  private renderHeader(name: string, description: string) {
+  protected renderHeader(name: string, description: string) {
+    const headingContainer = document.createElement('div');
+    headingContainer.classList.add('roman-arabic__heading-container');
+
     const h1Tag = document.createElement('h1');
     h1Tag.textContent = name;
     h1Tag.classList.add('heading', 'roman-arabic__heading');
@@ -34,24 +65,48 @@ export class RomanArabicView extends BaseView {
     pTag.textContent = description;
     pTag.classList.add('paragraph', 'roman-arabic__paragraph');
 
-    this.romanArabicSectionTag.appendChild(h1Tag);
-    this.romanArabicSectionTag.appendChild(pTag);
+    headingContainer.appendChild(h1Tag);
+    headingContainer.appendChild(pTag);
+
+    this.romanArabicSectionTag.prepend(headingContainer);
   }
 
-  private renderConvertUI(endpoint: string) {
-    const [httpMethod, url] = RomanArabicView.parseEndpoint(endpoint);
-
+  protected renderConversionUI(convertEndpoint: string, flushEndpoint: string) {
     const inputContainer = document.createElement('div');
+    inputContainer.classList.add('roman-arabic__input-container');
 
     const labelTag = document.createElement('label');
     labelTag.textContent = 'Enter Roman or Arabic Numeral:';
     labelTag.htmlFor = 'convert-input';
-    labelTag.classList.add('arabic-convert__label');
+    labelTag.classList.add('roman-arabic__label');
 
     const inputTag = document.createElement('input');
     inputTag.type = 'text';
     inputTag.id = 'convert-input';
     inputTag.classList.add('input', 'roman-arabic__input');
+
+    const btnsContainer = document.createElement('div');
+    btnsContainer.classList.add('roman-arabic__btn-container');
+
+    const convertBtn = this.generateConvertBtn(convertEndpoint, inputTag);
+    const flushBtn = this.generateFlushBtn(flushEndpoint);
+
+    btnsContainer.appendChild(convertBtn);
+    btnsContainer.appendChild(flushBtn);
+
+    inputContainer.appendChild(labelTag);
+    inputContainer.appendChild(inputTag);
+    inputContainer.appendChild(btnsContainer);
+
+    this.romanArabicSectionTag.prepend(inputContainer);
+  }
+
+  private generateConvertBtn(
+    convertEndpoint: string,
+    inputTag: HTMLInputElement
+  ) {
+    const [convertHttpMethod, convertUrl] =
+      RomanArabicView.parseEndpoint(convertEndpoint);
 
     const convertBtnTag = document.createElement('button');
     convertBtnTag.textContent = 'Convert';
@@ -61,20 +116,16 @@ export class RomanArabicView extends BaseView {
       'roman-arabic__btn--convert'
     );
     convertBtnTag.onclick = async () => {
-      const { data } = await request[httpMethod](
-        url.replace('{romanOrArabic}', inputTag.value)
+      const { data } = await request[convertHttpMethod](
+        convertUrl.replace('{romanOrArabic}', inputTag.value)
       );
       this.renderResults(data);
     };
 
-    inputContainer.appendChild(labelTag);
-    inputContainer.appendChild(inputTag);
-    inputContainer.appendChild(convertBtnTag);
-
-    this.romanArabicSectionTag.appendChild(inputContainer);
+    return convertBtnTag;
   }
 
-  private renderFlushUI(endpoint: string) {
+  private generateFlushBtn(endpoint: string) {
     const [method, url] = RomanArabicView.parseEndpoint(endpoint);
 
     const flushBtnTag = document.createElement('button');
@@ -87,21 +138,27 @@ export class RomanArabicView extends BaseView {
     flushBtnTag.onclick = async () => {
       await request[method](url);
       console.log('🗑️ Cache Flushed');
+
+      // Reset Table
+      this.table?.deleteAllRows();
     };
 
-    this.romanArabicSectionTag.appendChild(flushBtnTag);
+    return flushBtnTag;
   }
 
   protected renderResults(data: ConversionResult) {
     console.log('✅DATA RECEIVED: ', data);
-
-    // Render table of results
+    this.table?.addRow(data);
   }
 
-  private static parseEndpoint(endpoint: string): [HTTPMethod, string] {
+  protected static parseEndpoint(endpoint: string): [HTTPMethod, string] {
     const SPACE = ' ';
     const [method, url] = endpoint.split(SPACE);
     return [method.toLowerCase() as HTTPMethod, url];
+  }
+
+  protected resetSectionContainerHTML() {
+    this.romanArabicSectionTag.innerHTML = '';
   }
 }
 
