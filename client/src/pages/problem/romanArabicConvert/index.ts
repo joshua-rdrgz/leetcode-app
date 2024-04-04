7f16b0a753e43e7d60532161ca8cd0d9e87008b5
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-04-04 16:18:56 -0500

    rename import statements

--- client/src/pages/problem/romanArabicConvert/index.ts
+++ client/src/pages/problem/romanArabicConvert/index.ts
@@ -1,9 +1,9 @@
 import apiCache from '@/api/cache';
 import request from '@/api/request';
 import { SuiteData } from '@/models/SuiteData';
+import { RomanArabicController } from '@/pages/problem/romanArabicConvert/RomanArabicController';
+import { RomanArabicModel } from '@/pages/problem/romanArabicConvert/RomanArabicModel';
 import romanArabicView from '@/pages/problem/romanArabicConvert/RomanArabicView';
-import { RomanArabicController } from './RomanArabicController';
-import { RomanArabicModel } from './RomanArabicModel';
 
 export async function romanArabicConvertInit(suiteData: SuiteData) {
   const romanArabicModel = new RomanArabicModel(apiCache, request);
