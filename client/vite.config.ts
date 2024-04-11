8877b7c45869de19bddd995b6b3843924c7dae70
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-04-11 18:39:56 -0500

    Dockerize frontend/backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true,
